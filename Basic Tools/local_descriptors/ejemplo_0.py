5d864d028ab814d39753e072d22d2f96acf93c8e
Amitsuu <joaquinpereza96@gmail.com>
2019-10-18 01:03:36 -0300

    T3 progress 1

--- Basic Tools/local_descriptors/ejemplo_0.py
+++ Basic Tools/local_descriptors/ejemplo_0.py
@@ -7,17 +7,18 @@ Created on Wed Apr 11 15:50:08 2018
 
 This examples requires openCV
 """
-import cv2 
+import cv2
 import numpy as np
+
 
 if __name__ == '__main__':
     img = cv2.imread('../images/stitching/caso_1/1b.jpg')
-    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     sift = cv2.xfeatures2d.SIFT_create()
     kp = sift.detect(gray)
     print("n_kp: {}".format(len(kp)))
-    #kp, des = sift.compute(gray, kp)
-    cv2.drawKeypoints(gray,kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # kp, des = sift.compute(gray, kp)
+    cv2.drawKeypoints(gray, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     cv2.imshow("img1", img)
 
     cv2.waitKey()
--- Tarea 3/panoram.py
+++ Tarea 3/panoram.py
@@ -1,3 +1,45 @@
 
+import cv2
+import numpy as np
 
+
+def panoramic(img1, img2):
+    sift = cv2.xfeatures2d.SIFT_create(nOctaveLayers=6, sigma=1.6)
+
+    # make calculations on image1
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    kp1 = sift.detect(gray1)
+    kp1, des1 = sift.compute(gray1, kp1)
+
+    # make calculations on image2
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    kp2 = sift.detect(gray2)
+    kp2, des2 = sift.compute(gray2, kp2)
+
+    # match the multiple local descriptors
+    matcher = cv2.BFMatcher()
+    matches = matcher.knnMatch(des1, des2, k=2)
+
+    # Apply ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < 0.8 * n.distance:
+            good.append(m)
+
+    ##Calculando homografía para afinar correspondencias
+    if len(good) > 4:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 2.0)
+        matchesMask = mask.ravel().tolist()
+    else:
+        matchesMask = None
+
+    # Tengo el match con sift
+
+    img3 = 1 # Imagen irl
+
+
+    #
 
